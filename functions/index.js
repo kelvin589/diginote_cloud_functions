46457b759b7f488da50ec7bfb148c9c656f58625
kelvin589 <190081735@aston.ac.uk>
2022-03-18 15:48:29 +0000

    Notify devices to online statuss
    Set online status on call
    Also message user's devices to change in online/offline status

--- functions/index.js
+++ functions/index.js
@@ -60,3 +60,60 @@ exports.getUsersEmail = functions.https.onCall(async (data, context) => {
   const user = await admin.auth().getUser(userID);
   return user.email;
 });
+
+exports.notifyDevicesToOnlineStatus = functions.https.onCall(async (data, context) => {
+  // Retrieved parameters
+  const screenToken = data.token;
+  const isOnline = (data.isOnline === "true");
+  functions.logger.log("isOnline? " + isOnline);
+
+  const usersRef = admin.firestore().collection('users');
+  const screensRef = admin.firestore().collection('screens');
+  const screenInfoRef = admin.firestore().collection('screenInfo');
+  var deviceTokens = [];
+
+  // Set isOnline for this screen
+  await screenInfoRef.doc(screenToken).update({isOnline: isOnline});
+
+  // Find docs in collection 'users' where their 'screens' array contains this screen token
+  const snapshot = await usersRef.where('screens', 'array-contains', screenToken).get();
+  if (snapshot.empty) {
+    functions.logger.log("no matching docs");
+    return;
+  }
+
+  // Concatenate the user's devices FCMTokens
+  snapshot.forEach(doc => {
+    functions.logger.log("The doc's id");
+    functions.logger.log(doc.id);
+    functions.logger.log("The doc's data:");
+    functions.logger.log(doc.data().FCMTokens);
+    deviceTokens = deviceTokens.concat(doc.data()['FCMTokens']);
+  });
+
+  functions.logger.log("The device tokens to message:", deviceTokens);
+
+  const screensSnapshot = await screensRef.doc(screenToken).get();
+  if (screensSnapshot.empty) {
+    functions.logger.log("no matching docs for this screen");
+    return;
+  }
+  const screenName = screensSnapshot.data()["name"];
+
+  // Build the payload to send
+  const payload = {
+    "notification": {
+      "title": isOnline ? ("Screen online: " + screenName) : ("Screen offline: " + screenName),
+      "body": isOnline ? ("The screen has turn back online") : ("The screen has turned offline"),
+    },
+    "tokens": deviceTokens,
+  };
+
+  // Send message to all the devices
+  admin.messaging().sendMulticast(payload)
+    .then((response) => {
+      console.log(response.successCount + ' messages were sent successfully');
+    });
+
+  return `Successfully received: ${screenToken}`;
+});
