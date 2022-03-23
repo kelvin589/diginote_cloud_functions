174013f8841a4880b186258d67e7581abf3fbe42
kelvin589 <190081735@aston.ac.uk>
2022-03-23 16:33:20 +0000

    Extract function to notify devices to online status

--- functions/index.js
+++ functions/index.js
@@ -4,53 +4,21 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.notifyDevicesToLowBattery = functions.https.onCall(async (data, context) => {
-  // Grab the text parameter.
+  // Grab the parameters
   const screenToken = data.token;
   const batteryLevel = data.batteryLevel;
 
-  var deviceTokens = [];
-
-  // Find docs in collection 'users' where their 'screens' array contains this screen token
-  const usersRef = admin.firestore().collection('users');
-  const snapshot = await usersRef.where('screens', 'array-contains', screenToken).get();
-  if (snapshot.empty) {
-    functions.logger.log("no matching docs");
-    return;
-  }
-
-  // Concatenate the user's devices FCMTokens
-  snapshot.forEach(doc => {
-    functions.logger.log("The doc's id");
-    functions.logger.log(doc.id);
-    functions.logger.log("The doc's data:");
-    functions.logger.log(doc.data().FCMTokens);
-    deviceTokens = deviceTokens.concat(doc.data()['FCMTokens']);
-  });
-
-  functions.logger.log("The device tokens to message:", deviceTokens);
-
-  const screensRef = admin.firestore().collection('screens');
-  const screensSnapshot = await screensRef.doc(screenToken).get();
-  if (screensSnapshot.empty) {
-    functions.logger.log("no matching docs for this screen");
-    return;
-  }
-  const screenName = screensSnapshot.data()["name"];
-
   // Build the payload to send
+  const screenName = await getScreenName(screenToken);
   const payload = {
     "notification": {
       "title": "Low Battery for screen: " + screenName,
       "body": "Remaining battery percentage is " + batteryLevel,
     },
-    "tokens": deviceTokens,
   };
 
   // Send message to all the devices
-  admin.messaging().sendMulticast(payload)
-    .then((response) => {
-      console.log(response.successCount + ' messages were sent successfully');
-    });
+  await notifyDevices(screenToken, payload)
 
   return `Successfully received: ${screenToken}`;
 });
@@ -65,28 +33,42 @@ exports.onIsOnlineChanged = functions.database.ref('/status/{screenToken}').onUp
   async (change, context) => {
     const after = change.after.val();
     const isOnline = (after.isOnline === "true");
+    const screenToken = context.params.screenToken;
 
-    const screenInfoFirestoreRef = admin.firestore().collection('screenInfo').doc(context.params.screenToken);
-    await screenInfoFirestoreRef.update({isOnline: isOnline});
+    const screenInfoFirestoreRef = admin.firestore().collection('screenInfo').doc(screenToken);
+    await screenInfoFirestoreRef.update({ isOnline: isOnline });
+
+    // Build the payload to send
+    const screenName = await getScreenName(screenToken);
+    const payload = {
+      "notification": {
+        "title": isOnline ? ("Screen online: " + screenName) : ("Screen offline: " + screenName),
+        "body": isOnline ? ("The screen has reconnected.") : ("The screen has disconnected."),
+      },
+    };
+
+    // Send message to all the devices
+    await notifyDevices(screenToken, payload);
   }
 );
 
-exports.notifyDevicesToOnlineStatus = functions.https.onCall(async (data, context) => {
-  // Retrieved parameters
-  const screenToken = data.token;
-  const isOnline = (data.isOnline === "true");
-  const message = data.message;
-  functions.logger.log("isOnline? " + isOnline);
-
-  const usersRef = admin.firestore().collection('users');
+async function getScreenName(screenToken) {
   const screensRef = admin.firestore().collection('screens');
-  const screenInfoRef = admin.firestore().collection('screenInfo');
+  const screensSnapshot = await screensRef.doc(screenToken).get();
+
+  if (screensSnapshot.empty) {
+    functions.logger.log("no matching docs for this screen");
+    return;
+  }
+
+  return screensSnapshot.data()["name"];
+}
+
+async function notifyDevices(screenToken, payload) {
   var deviceTokens = [];
 
-  // Set isOnline for this screen
-  await screenInfoRef.doc(screenToken).update({isOnline: isOnline});
-
   // Find docs in collection 'users' where their 'screens' array contains this screen token
+  const usersRef = admin.firestore().collection('users');
   const snapshot = await usersRef.where('screens', 'array-contains', screenToken).get();
   if (snapshot.empty) {
     functions.logger.log("no matching docs");
@@ -103,28 +85,38 @@ exports.notifyDevicesToOnlineStatus = functions.https.onCall(async (data, contex
   });
 
   functions.logger.log("The device tokens to message:", deviceTokens);
-
-  const screensSnapshot = await screensRef.doc(screenToken).get();
-  if (screensSnapshot.empty) {
-    functions.logger.log("no matching docs for this screen");
-    return;
-  }
-  const screenName = screensSnapshot.data()["name"];
-
-  // Build the payload to send
-  const payload = {
-    "notification": {
-      "title": isOnline ? ("Screen online: " + screenName) : ("Screen offline: " + screenName),
-      "body": isOnline ? ("The screen has turn back online. " + message) : ("The screen has turned offline. " + message),
-    },
-    "tokens": deviceTokens,
-  };
+  payload = Object.assign(payload, { "tokens": deviceTokens });
 
   // Send message to all the devices
   admin.messaging().sendMulticast(payload)
     .then((response) => {
       console.log(response.successCount + ' messages were sent successfully');
     });
+}
+
+// Manually set device online status
+exports.notifyDevicesToOnlineStatus = functions.https.onCall(async (data, context) => {
+  // Grab the parameters
+  const screenToken = data.token;
+  const isOnline = (data.isOnline === "true");
+  const message = data.message;
+  functions.logger.log("isOnline? " + isOnline);
+
+  // Set isOnline for this screen
+  const screenInfoRef = admin.firestore().collection('screenInfo');
+  await screenInfoRef.doc(screenToken).update({ isOnline: isOnline });
+
+  // Build the payload to send
+  const screenName = await getScreenName(screenToken);
+  const payload = {
+    "notification": {
+      "title": isOnline ? ("Screen online: " + screenName) : ("Screen offline: " + screenName),
+      "body": isOnline ? ("The screen has turned back online. " + message) : ("The screen has turned offline. " + message),
+    },
+  };
+
+  // Send message to all the devices
+  await notifyDevices(screenToken, payload);
 
   return `Successfully received: ${screenToken}`;
 });
